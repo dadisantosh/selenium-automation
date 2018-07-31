759c6a7e08bc64177c17ea2c565dbb56ed07cc56
Amrita-Incture <amrita.rani@incture.com>
2018-07-31 12:14:48 +0530

    changed paths

--- src/utility/Constants.java
+++ src/utility/Constants.java
@@ -5,8 +5,8 @@ public class Constants {
 	public static final String reportName = "Zuellig Pharma";
 	public static final int ThinkTime = 2;
 	public static final String URL = "https://ereturn-c8e00d73c.dispatcher.ap1.hana.ondemand.com/index.html?hc_reset#";
-	public static final String xcelPath = "D://Incture//AutomationFramWork//src//testData//";
+	public static final String xcelPath = "$WORKSPACE/src/testData";
 	public static final String xcelfile = "CopyProjectTestData.xlsx";
-	public static final String xPathfile = "D://ZUelig Pharma//Zuelig Pharma//src//objectRepository//Login_LogOut.properties";
+	public static final String xPathfile = "$WORKSPACE/src/objectRepository/Login_LogOut.properties";
 	
 }
